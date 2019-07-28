8bcb7bf2631297fc5f741e9f48cc1447e335f187
ozuit <tantd2211@gmail.com>
2019-07-28 16:09:47 +0700

    add topic in migration

--- db/migrations/20190723044426_sensor.php
+++ db/migrations/20190723044426_sensor.php
@@ -13,6 +13,7 @@ class Sensor extends AbstractMigration
             ->addColumn('name', 'string', ['null' => true, 'default' => null])
             ->addColumn('active', 'boolean', ['null' => false, 'default' => true])
             ->addColumn('room_id', 'integer', ['null' => true, 'default' => null])
+            ->addColumn('topic', 'string', ['unique' => true])
             ->addForeignKey('room_id', 'room', 'id', ['delete' => 'SET NULL'])
             ->addColumn('created_at', 'timestamp', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('updated_at', 'timestamp', ['null' => true, 'default' => null])

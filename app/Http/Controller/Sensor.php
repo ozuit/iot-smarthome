836055204014c0fad85eaee3ee4a3e91a09c5283
ozuit <tantd2211@gmail.com>
2019-08-04 19:29:29 +0700

    update api publish control device

--- app/Http/Controller/Sensor.php
+++ app/Http/Controller/Sensor.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controller;
 
 use App\Service\SensorService;
+use Symfony\Component\HttpFoundation\Response;
+use Bluerhinos\phpMQTT;
 
 class Sensor extends Api
 {
+    protected $actions = [
+        'update',
+    ];
+
     protected function getService() : SensorService
     {
         return $this->get(SensorService::class);
@@ -19,5 +25,30 @@ class Sensor extends Api
             'put' => ['update', 'sensor'],
             'delete' => ['delete', 'sensor'],
         ];
+    }
+
+    protected function update(): Response
+    {
+        $server = env('MQTT_SERVER');
+        $port = env('MQTT_PORT');
+        $client_id = env('MQTT_CLIENT_ID');
+
+        $mqtt = new phpMQTT($server, $port, $client_id);
+        $topic = $this->getJsonData('topic', 'trash');
+        $payload = $this->getJsonData('payload', '');
+
+        if ($mqtt->connect()) {
+            $mqtt->publish($topic, $payload);
+            $mqtt->close();
+            
+            return $this->json([
+                'status' => true,
+            ]);
+        } else {
+            return $this->json([
+                'status' => false,
+                'message' => 'Time out!'
+            ]);
+        }
     }
 }

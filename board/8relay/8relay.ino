43c25b281fb167c989921970134f7e708ed44534
ozuit <tantd2211@gmail.com>
2019-08-04 22:00:17 +0700

    update relay control

--- board/8relay/8relay.ino
+++ board/8relay/8relay.ino
@@ -48,7 +48,7 @@ void setup_wifi() {
 }
 
 void setup() {
-  
+
   pinMode(PIN_D0, OUTPUT);
   digitalWrite(PIN_D0, LOW);
   
@@ -100,18 +100,17 @@ boolean verify(byte* payload)
   String hash = str_payload.substring(0,32);
   String ts = str_payload.substring(33,43);
   unsigned long ts_long = ts.toInt();
-  String data = str_payload.substring(44);
 
   if (now_ts - ts_long > timeout) {
       return false;
   }
 
-  String hash_data = str_payload.substring(33) + '|' + secret_key;
-  char * cstr = new char [hash_data.length()+1];
+  String hash_data = str_payload.substring(33, 45) + '|' + secret_key;
+  char *cstr = new char [hash_data.length()+1];
   strcpy (cstr, hash_data.c_str());
   unsigned char* re_hash = MD5::make_hash(cstr);
   char *md5str = MD5::make_digest(re_hash, 16);
-
+  
   if (hash == String(md5str)) {
     return true;
   }
@@ -124,16 +123,70 @@ void callback(char* topic, byte* payload, unsigned int length) {
   
   if (verify(payload)) {
     String str_payload = String((char*)payload);
-    String state = str_payload.substring(44);
+    String state = str_payload.substring(44,45);
     Serial.println(topicStr + " " + state);
     
-    if (topicStr == "smarthome/living-room/light/device1") // Led 8 on board 8 relay 
+    if (topicStr == "smarthome/living-room/light/device1") // Relay number 8
     {
        if(state == "1") {
           digitalWrite(PIN_D0, HIGH);
        }
        else if (state == "0") {
           digitalWrite(PIN_D0, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/living-room/light/device2") // Relay number 7
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D1, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D1, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/living-room/fan/device1") // Relay number 6
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D2, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D2, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/bed-room/light/device1") // Relay number 5
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D3, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D3, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/bed-room/fan/device1") // Relay number 4
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D4, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D4, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/bath-room/light/device1") // Relay number 3
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D5, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D5, LOW);
+       }
+    }
+    else if (topicStr == "smarthome/kitchen/light/device1") // Relay number 2
+    {
+       if(state == "1") {
+          digitalWrite(PIN_D6, HIGH);
+       }
+       else if (state == "0") {
+          digitalWrite(PIN_D6, LOW);
        }
     }
   } else {

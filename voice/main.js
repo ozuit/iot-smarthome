bc6007d3734834b7bf5eb42f1349b1a402822087
ozuit <tantd2211@gmail.com>
2019-09-29 22:38:07 +0700

    update text to speech

--- voice/main.js
+++ voice/main.js
@@ -4,10 +4,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({ limit: '50mb' }))
 
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
+const textToSpeech = require('@google-cloud/text-to-speech');
 const fs = require('fs');
 
 app.get('/', (req, res) => {
@@ -16,7 +17,8 @@ app.get('/', (req, res) => {
 
 app.post('/speech-to-text', async (req, res) => {
   // Creates a client
-  const client = new speech.SpeechClient();
+  const clientS2T = new speech.SpeechClient();
+  const clientT2S = new textToSpeech.TextToSpeechClient();
 
   // The name of the audio file to transcribe
   const fileName = './test.wav';
@@ -33,18 +35,29 @@ app.post('/speech-to-text', async (req, res) => {
     encoding: 'LINEAR16',
     languageCode: 'vi-VN',
   };
-  const request = {
+  const requestS2T = {
     audio: audio,
     config: config,
   };
 
   // Detects speech in the audio file
-  const [response] = await client.recognize(request);
-  const transcription = response.results
+  const [responseS2T] = await clientS2T.recognize(requestS2T);
+  const transcription = responseS2T.results
     .map(result => result.alternatives[0].transcript);
- 
+
+  const requestT2S = {
+    input: { text: transcription[0] },
+    // Select the language and SSML Voice Gender (optional)
+    voice: { languageCode: 'vi-VN', ssmlGender: 'NEUTRAL' },
+    // Select the type of audio encoding
+    audioConfig: { audioEncoding: 'MP3' },
+  };
+
+  // Performs the Text-to-Speech request
+  const [responseT2S] = await clientT2S.synthesizeSpeech(requestT2S);
+
   // Send response
-  res.send(transcription[0])
+  res.send(responseT2S.audioContent.toString('base64'))
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
